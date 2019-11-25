a1a69a88b3215f3ab28f50a3691a0b937389173d
setiyawan <setiyawan@localhost.localdomain>
2019-11-25 19:52:06 +0700

    screenlogin

---
+++ src/app/Koneksi.java
@@ -0,0 +1,25 @@
+package app;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Koneksi {
+
+    public static Connection MySQL(){
+        try {
+            MysqlDataSource ds = new MysqlDataSource();
+            ds.setUser("root");
+            ds.setPassword("");
+            ds.setServerName("localhost");
+            ds.setDatabaseName("perpustakaan");
+            ds.setPortNumber(3306);
+            Connection c = ds.getConnection();
+            return c;
+            
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+    }
+}
